762b345a59d2e9f358cd822600ba6c60e7926ca2
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- pager/doc.go
+++ pager/doc.go
@@ -6,6 +6,5 @@ pager.Start method and if either of the writers is connected to a terminal
 then it will start a pager command and insert it between the writer and the
 terminal. The Start method returns a pager and you must call the Done method
 on the returned pager.
-
 */
 package pager
